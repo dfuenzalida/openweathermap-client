b64f6477237bed75134ddcb4f350d16a8d40d18a
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2014-02-28 23:28:48 -0300

    First import

---
+++ test/openweatherdb/core_test.clj
@@ -0,0 +1,7 @@
+(ns openweatherdb.core-test
+  (:require [clojure.test :refer :all]
+            [openweatherdb.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
